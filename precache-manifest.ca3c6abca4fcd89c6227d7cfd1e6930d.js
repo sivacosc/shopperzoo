951a5f0ca2ca68d07de4319234d0b5fd9b69adb1
Sivaramakrishnan Kasiviswanathan <vishnu2503@gmail.com>
2019-09-11 02:55:14 +0530

    Updates

---
+++ precache-manifest.ca3c6abca4fcd89c6227d7cfd1e6930d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acee990c8c77e3b7b1d648210b7743fc",
+    "url": "/shopperzoo/index.html"
+  },
+  {
+    "revision": "3a5f147236d08f9a9ad8",
+    "url": "/shopperzoo/static/css/main.ab8e8818.chunk.css"
+  },
+  {
+    "revision": "f58a3b9f155c086ff9a1",
+    "url": "/shopperzoo/static/js/2.83ad1c11.chunk.js"
+  },
+  {
+    "revision": "3a5f147236d08f9a9ad8",
+    "url": "/shopperzoo/static/js/main.e3f4a20c.chunk.js"
+  },
+  {
+    "revision": "d6b9f6e60dd3349068c3",
+    "url": "/shopperzoo/static/js/runtime~main.a2e917bf.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shopperzoo/static/media/crown.ed28accb.svg"
+  }
+]);
