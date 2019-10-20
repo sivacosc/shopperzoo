85543e646b2a0829edc4926100f6f15297e0405c
Sivaramakrishnan Kasiviswanathan <vishnu2503@gmail.com>
2019-10-21 01:15:37 +0530

    Added Add-to-cart functionality

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const cartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEMS: 'ADD_ITEM'
 };
 
 export default cartActionTypes;
