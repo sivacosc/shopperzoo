64ff34dca7048baacfea2a7157c17b1193cf52df
Sivaramakrishnan Kasiviswanathan <vishnu2503@gmail.com>
2019-11-18 01:23:28 +0530

    Added remove functionality to the checkout page

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,14 @@ export const toggleCartHidden = () => ({
 export const addItem = item => ({
     type: cartActionTypes.ADD_ITEMS,
     payload: item
-})
+});
+
+export const removeItem = item => ({
+    type: cartActionTypes.REMOVE_ITEM,
+    payload: item
+});
+
+export const clearItemFromCart = item => ({
+   type: cartActionTypes.CLEAR_ITEM_FROM_CART,
+   payload: item
+});
