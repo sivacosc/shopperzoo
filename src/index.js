73b0636c3d947cd91733839fc387b19df00a031e
Sivaramakrishnan Kasiviswanathan <vishnu2503@gmail.com>
2019-11-18 10:07:27 +0530

    Added Local storage using react-persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -10,7 +11,11 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
         <Provider store={store}>
             <HashRouter basename='/'>
-                <App />
+                <BrowserRouter>
+                    <PersistGate persistor={ persistor }>
+                        <App />
+                    </PersistGate>
+                </BrowserRouter>
             </HashRouter>
         </Provider>,
         document.getElementById('root')
